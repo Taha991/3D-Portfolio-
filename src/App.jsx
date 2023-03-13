673897b5f5811e6d7124a07e6492529505dd37f3
Taha991 <tahamohamedman@gmail.com>
2023-03-13 09:52:01 +0200

    adding styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
+import styled from "styled-components";
 import { Contact } from "../components/Contact"
 import { Main } from "../components/Main"
 import { Projects } from "../components/Projects"
 import { Who } from "../components/Who"
 
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+
+`;
+ 
+
+
 function App() {
 
   return (
     <div>
-      <Main/>
+      <Container>
+      <Main/> 
       <Who/>
       <Projects/>
       <Contact/>
+      </Container>
+      
     </div>
   )
 }
